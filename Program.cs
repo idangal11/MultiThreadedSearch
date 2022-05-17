d2647701f7c3b4f11545b83b53d85c8eb20581a5
idang <idang@DESKTOP-5SOI3BC>
2022-05-17 19:29:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MultiThreadedSearch;
+
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+
+        private static void Main(string[] args)
+        {
+
+            int nThreads = int.Parse(args[3]);
+            int delta = int.Parse(args[3]);
+            List<string> text = File.ReadAllLines(args[0]).ToList();
+            //@"C:\Users\idang\OneDrive\Documents\test.txt"
+            string StringToSearch = args[1];
+          
+            int div = 0;
+            div = text.Count / nThreads;
+            
+
+            findWords.lines = text;
+            findWords.startLine = 0;
+            findWords.endLine = div;
+            findWords.StringToSearch = StringToSearch;
+            findWords.delta=delta;
+
+            for (int i = 0; i < nThreads; i++)
+            {
+                Thread thread = new Thread(findWords.findCorrectWords);               
+                thread.Start();
+
+                Thread.Sleep(2000);
+                findWords.startLine += div;
+                findWords.endLine += div;             
+
+            }
+        }
+    } 
+}
---
+++ findWords.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadedSearch
+{
+    internal class findWords
+    {
+        public static List<string> lines = new List<string>();
+        public static int startLine;
+        public static int endLine;
+        public static int delta;
+        public static String StringToSearch;
+
+        public static void findCorrectWords()
+        {
+            
+            List<char> str = new List<char>();
+             int c = 0;
+             
+             int d = 0;
+             int flag = 1;
+             int j;
+             int i;
+
+           for(int s=0; s< StringToSearch.Length;s++)
+            {
+                str.Add(StringToSearch[s]);
+            }
+
+            for (i = startLine; i < endLine; i++)
+            {          
+                for (j = 0; j < lines[i].Length; j++)
+                {
+                   
+                    if (lines[i][j] == str[0])
+                    {
+                        int line = i;
+                        int col = j;
+
+                        while (c < str.Count)
+                        {
+                            if (flag == 1) { c = 1 ; flag = 0; }
+                            d = delta;
+                            if(d == 0)
+                            {                      
+                                j++;
+                                if (flag == 0 && !(lines[i][j] == str[c])) { c = 0; flag = 1; break; } 
+                            }
+
+                            else
+                            {                               
+                                if (j+1 +d > lines[i].Length)
+                                {
+                                    d -= lines[i].Length - j;
+                                   
+                                    while(d > lines[i + 1].Length)
+                                    {                                       
+                                            d -= lines[i + 1].Length + 1;
+                                            i++; j = -1;
+          
+                                    }
+                                    i++; j = -1;
+                                }
+                                if(j+1+d == lines[i].Length)
+                                {
+                                    break;
+                                }
+                                                           
+                                if (flag == 0 && !(lines[i][j + d + 1] == str[c])) { c = 0; flag = 1; break; } else { j = (j + d + 1); d = delta; }
+                            }
+                           
+                            c++;
+                            if (c == str.Count)
+                            {               
+                                c = 0;
+                                flag = 1;
+                                Console.WriteLine("[" + line + "," + col + "]");
+                                break;
+                            }
+                        }
+                    }
+                }
+            }           
+
+        }
+        
+
+    }
+
+}
+                               
